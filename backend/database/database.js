d708aa542888683f6c6f60a5fb185cfe95f9867b
Marta Grazia <23011076@uhi.ac.uk>
2025-09-22 15:40:26 +0100

    updated db connection file with source code reference

--- backend/database/database.js
+++ backend/database/database.js
@@ -5,8 +5,9 @@ const db = new sqlite.Database('gamerater_db.db', (error) => {
     if (error) {
         return console.error(error.message);
     }
+    console.log("Connected to DB.");
 });
 
-console.log("Connected to DB.");
-
 module.exports = db;
+
+//example source https://www.digitalocean.com/community/tutorials/how-to-use-sqlite-with-node-js-on-ubuntu-22-04#step-2-connecting-to-an-sqlite-database
