a6a40a9d0eb1903fb8b2681bca05c757c9db09e7
Marta Grazia <23011076@uhi.ac.uk>
2025-09-22 23:13:08 +0100

    added rating and game routes placeholders

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,5 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const validateToken = require('../middleware/auth');
+
+//protect all game routes
+router.use(validateToken);
+router.get('/search', (req,res)=>{
+    res.json({message:'Search endpoint'});
+});
+router.get('/user-ratings', (req, res) => {
+    res.json({message: "all ratings endpoint"});
+});
+router.post('/rate', (req, res) => {
+    res.json({message:'send rate endpoint'});    
+});
+router.put('/update-rate/:id', (req, res) => {
+    res.json({message:'Update rate endpoint'});    
+});
+router.delete('/delete/:id', (req,res) => {
+    res.json({message: 'Delete rating endpoint'});
+});
+
 
 module.exports = router;
