45a52e24ece4b985dc061dd807a666567328bb20
Marta Grazia <23011076@uhi.ac.uk>
2025-09-23 20:06:50 +0100

    corrected db path and added helper methods wrappers for asyc

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,13 +1,39 @@
 const sqlite = require('sqlite3').verbose();
 
 
-const db = new sqlite.Database('gamerater_db.db', (error) => {
+exports.db = new sqlite.Database('./database/gamerater_db.db', sqlite.OPEN_READWRITE, (error) => {
     if (error) {
         return console.error(error.message);
     }
     console.log("Connected to DB.");
 });
 
-module.exports = db;
+exports.fetchAll = async (db, sql, params) => {
+    return new Promise ((resolve, reject) => {
+        db.all(sql, params, (err, rows)=>{
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+};
+
+exports.fetchFirst = async (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) reject(err);
+      resolve(row);
+    });
+  });
+};
+
+exports.runAsync = async(db, sql, params) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, function(err){
+            if (err) reject(err);
+            resolve(this);
+        })
+    })
+}
 
 //example source https://www.digitalocean.com/community/tutorials/how-to-use-sqlite-with-node-js-on-ubuntu-22-04#step-2-connecting-to-an-sqlite-database
+//method wrapper source https://www.sqlitetutorial.net/sqlite-nodejs/query/
