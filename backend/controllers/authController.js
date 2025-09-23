3b17431068be55d5ffb6e98ce220efadb654a6d6
Marta Grazia <23011076@uhi.ac.uk>
2025-09-23 20:05:50 +0100

    updated authController to use async helper methods

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,48 +1,46 @@
-const db = require('../database/database');
+const {db, fetchAll, fetchFirst, runAsync} =require('../database/database');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = process.env;
 
 exports.register = async (req, res) => {
     const {username, email, password, confirmedPassword } = req.body;
+    //input validation
+    if (!username || !email || !password || !confirmedPassword) {
+    return res.status(400).json({ message: 'all fields are required' });
+    }
+    //pwd length requirement
+    if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ message: 'password must be at least 8 characters' });
+    }
+    if (password !== confirmedPassword) {
+    return res.status(400).json({ message: 'passwords do not match' });
+    }
     try {
         //check if username is already used
-        let nameSql = `select * from users where name = ?`;
-        db.get(nameSql, [username], async (err, existingUsername) =>{
-            if (err) {
-                return res.status(500).json({message: "db error"});
-            }
-            if(existingUsername) {
-                return res.status(400).json({message: "username already exists"});
-            }
-            //checks if email is already used
-            let emailSql = `select * from users where email = ?`;
-            db.get(emailSql, [email], async(err, existingEmail) => {
-                if(err) {
-                    return res.status(500).json({message: "db error"});
-                }
-                if(existingEmail){
-                    return res.status(400).json({message: "email already used"});
-                }
-                if(confirmedPassword === password){
-                    //hash password
-                    const hashedPassword = await bcrypt.hash(password, 10);
-
-                    //add user to table
-                    let addSql = `insert into users(name, email, password) values (?,?,?)`;
-                    let values = [username, email, hashedPassword];
-                    db.run(addSql, values, (err) => {
-                        if (err){
-                            return res.status(500).json({message: "failed user creation"});
-                        }
-                        return res.status(201).json({message: "user created"});
-                    });
-                } else {
-                    return res.status(400).json({message: 'passwords do not match'});
-                }                
-            });
-        });                
+        let nameSql = `select * from users where username = ?`;
+        const user = await fetchFirst(db, nameSql, [username]); 
+        if(user) {
+            return res.status(400).json({message: "username already exists"});
+        }
+        //checks if email is already used
+        let emailSql = `select * from users where email = ?`;
+        const existingEmail = await fetchFirst(db,emailSql, [email]);
+        if(existingEmail){
+            return res.status(400).json({message: "email already used"});
+        }
+        
+        //hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        //add user to table
+        let addSql = `insert into users(username, email, password) values (?,?,?)`;
+        let values = [username, email, hashedPassword];
+        await runAsync(db, addSql, values);                
+        return res.status(201).json({message: "user created"});                                
     } catch (error) {
+        console.error(err);
         res.status(500).json({message: "server error", error: error.message});
     }
 }
@@ -51,31 +49,28 @@ exports.login = async (req, res) => {
     const {email, password} = req.body;
     try {
         //look for user and get password
-        let getUserSql = `select id, name, email, password from users where email = ?`;
-        db.get(getUserSql, [email], async (err, user) =>{
-            if (err) {
-                return res.status(500).json({message:'db error', error: err.message});
-            }
-            if(user){
-                const isMatch = await bcrypt.compare(password, user.password);
-                if(!isMatch){
-                    return res.status(400).json({message: 'Wrong email or password'});
-                }
-                const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: "1d"});
-                return res.status(200).json({
-                    message: 'log in successful',
-                    token,
-                    user: {
-                        id: user.id,
-                        name: user.name,
-                        email: user.email
-                    }
-                });
-            } else {
+        let getUserSql = `select id, username, email, password from users where email = ?`;
+        const user = await fetchFirst(db, getUserSql, [email]);            
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch){
                 return res.status(400).json({message: 'Wrong email or password'});
             }
-        })
+            const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: "1d"});
+            return res.status(200).json({
+                message: 'log in successful',
+                token,
+                user: {
+                    id: user.id,
+                    username: user.username,
+                    email: user.email
+                }
+            });
+        } else {
+            return res.status(400).json({message: 'Wrong email or password'});
+        }        
     } catch (error){
+        console.error(err);
         res.status(500).json({message: "server error", error: error.message});
     }
 }
