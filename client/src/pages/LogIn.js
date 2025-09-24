a855b107715b4b42a6e187401441789dafc17d7f
Marta Grazia <23011076@uhi.ac.uk>
2025-09-25 00:10:46 +0100

    added login and registration pages, forms and fetch logic

---
+++ client/src/pages/LogIn.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    const navigate = useNavigate();
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    async function handleLogin(e){
+        e.preventDefault();
+        const res = await fetch("http://localhost:8080/api/auth/login",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }) 
+
+        const data = await res.json();
+        if(res.ok) {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("user", JSON.stringify(data.user));
+            navigate("/homePage");
+        } else {
+            alert(data.message);
+        }
+    }
+
+    return (    
+    <div className='page-container'>
+      
+      <div className='header'>
+        <h1 className='title'>
+          Log In to GameRater
+        </h1>      
+      </div>  
+      <div className="content-wrap">
+        <div className='auth-form'>
+          <form>
+            <label>Email or Username</label>
+            <input type='text' placeholder='Enter Email...' value={email} onChange={e=> setEmail(e.target.value)}/>
+            <br />
+            <label>Password</label>
+            <input type='password' placeholder='Enter Password...' value={password} onChange={e=> setPassword(e.target.value)}/>
+            <button type="submit" onClick={handleLogin}>Log In</button>
+          </form>
+        </div>
+      </div>
+      
+    </div>
+   )
+}
+export default Login;
