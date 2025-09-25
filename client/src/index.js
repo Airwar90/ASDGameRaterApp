dc06e6968f51c6b0cef3824f6ea4f58577de83bd
Marta Grazia <23011076@uhi.ac.uk>
2025-09-25 22:33:48 +0100

    added routes for new pages

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import HomePage from './pages/HomePage';
+import LogIn from './pages/LogIn';
+import Register from './pages/Register';
+import AddRating from './pages/AddRating';
+import UserRatings from './pages/UserRatings';
+import UpdateRating from './pages/UpdateRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/userRatings" element={<UserRatings />} />
+        <Route path="/addRating" element={<AddRating />} />
+        <Route path="/updateRating" element={<UpdateRating />} />
+        
+      </Routes>      
+    </HashRouter>
   </React.StrictMode>
 );
 
