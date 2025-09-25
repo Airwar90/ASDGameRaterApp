3c8864da85bb71a984f83a26ea30bc6cdc491e25
Marta Grazia <23011076@uhi.ac.uk>
2025-09-25 22:29:26 +0100

    updater landing page to use unordered list, updated header class name

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,15 +4,15 @@ import {Link} from 'react-router-dom'
 function Landing() {
     return (
         <div className='page-container'>
-            <div className='header'>
-                <h1 className='Title'>Welcome to GameRater</h1>
+            <div className='landing-header'>
+                <h1 className='Title'>GameRater</h1>
                 <h3 className='Subtitle'>Register or Log In</h3>
             </div>    
             <div className='content-wrap'>
-                <ol className='simple-vertical-list'>
+                <ul className='simple-vertical-list'>
                     <li><Link to={"/register"}>Register</Link></li>
                     <li><Link to={"/login"}>Log In</Link></li>
-                </ol>
+                </ul>
             </div>
         </div>
     );
